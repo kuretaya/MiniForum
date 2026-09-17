7b71d806e7b1dd3302c5643a3f38f587c57ff62a
kuretaya <paulin.dossi@gmail.com>
2026-09-17 15:18:47 +0200

    modification css (background) + js (liens button)

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click',() => {
-    alert('Bonjour depuis JavaScript !');
+btn.addEventListener('click', () => {
+    window.open(
+        'https://www.youtube.com/watch?v=Y80juYcu3ZI&list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3',
+        '_blank'
+    );
 });
